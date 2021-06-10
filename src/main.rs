b40df454a52b9f42191cb706b3744ae572a00ee1
elpnt <quantumular@gmail.com>
2021-06-10 17:44:26 +0900

    Display a text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::event::{read, Event, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use tui::layout::{Constraint, Layout};
+use tui::widgets::{Block, Borders, Paragraph};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> anyhow::Result<()> {
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
+
+            // Surrounding block
+            let block = Block::default().borders(Borders::ALL);
+            f.render_widget(block, size);
+
+            // Body
+            let chunks = Layout::default()
+                .direction(tui::layout::Direction::Vertical)
+                .margin(2)
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .split(f.size());
+            let paragraph = Paragraph::new("Hello").alignment(tui::layout::Alignment::Center);
+            f.render_widget(paragraph, chunks[0]);
+        })?;
+        let event = read().unwrap();
+        if event == Event::Key(KeyCode::Char('q').into()) {
+            disable_raw_mode()?;
+            execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+            break;
+        }
+    }
+
+    Ok(())
 }
